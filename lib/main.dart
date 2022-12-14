2d24456d01fb98685c61c897fcd2384e71514b72
Jan <cs31a8balbon@gmail.com>
2022-12-14 17:27:39 +0800

    splash view added

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:loginuicolors/login.dart';
 import 'package:loginuicolors/register.dart';
 
-void main() {
-  runApp(MaterialApp(
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyLogin(),
     routes: {
       'register': (context) => MyRegister(),
       'login': (context) => MyLogin(),
-    },
-  ));
+      },
+    )
+  );
 }
